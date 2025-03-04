05a6745b24122031b626f347d025d1425d3a7090
GJC <599953528@qq.com>
2025-03-04 15:12:57 +0800

    feat(Springboot):初始化springboot服务 1.新增sayHello对外API接口 2.修改配置文件application.yml，将http端口改为8888

---
+++ src/main/java/mangosteen/controller/HelloController.java
@@ -0,0 +1,30 @@
+package mangosteen.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Hello Controller
+ */
+// 对外提供一个controller
+@RestController
+public class HelloController {
+    // 将该方法暴露出去
+
+    /**
+     * Say Hello API.
+     * @param name name
+     * @return hello string
+     */
+    //http://localhost:8080/v1/hello/name
+    @GetMapping(path = "v1/hello/{name}")
+    public String sayHello(@PathVariable("name") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    //http://localhost:8080/v1/greeting?name=zhangsan&id=100
+    @GetMapping(path = "v1/greeting")
+    public String greeting(@RequestParam("name") String name,
+                           @RequestParam("id") Long id) {
+        return String.format("Hello %s with %s", name,id);
+    }
+}
