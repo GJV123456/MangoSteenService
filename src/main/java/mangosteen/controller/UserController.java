6f205b273694c5a80ad794a63c0dfd54358e8f41
GJC <599953528@qq.com>
2025-03-06 10:52:41 +0800

    feat(Springboot-Mybatis): 1.springboot连接mysql 2.使用Springboot与Mybatis集成 3.接口测试正确,json返回值与预期一致

---
+++ src/main/java/mangosteen/controller/UserController.java
@@ -0,0 +1,25 @@
+package mangosteen.controller;
+
+import mangosteen.mapper.UserInfoMapper;
+import mangosteen.model.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserInfoMapper userInfoMapper;
+
+    // 将UserInfoMapper进行构造器注入
+    @Autowired
+    public UserController(UserInfoMapper userInfoMapper) {
+        this.userInfoMapper = userInfoMapper;
+    }
+
+    @GetMapping(path = "v1.0/users/{id}")
+    UserInfo getUserInfoById(@PathVariable Long id) {
+        return userInfoMapper.getUserInfoByUserId(id);
+    }
+}
---
+++ src/main/java/mangosteen/mapper/UserInfoMapper.java
@@ -0,0 +1,21 @@
+package mangosteen.mapper;
+
+import mangosteen.model.UserInfo;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * ORM:对象（data model UserInfo.java）映射到数据库表（ms_userinfo）
+ */
+
+//添加 Mapper注解 让mybatis识别出UserInfoMapper是做映射的
+    // 创建Mapper接口
+@Mapper
+public interface UserInfoMapper {
+    //SELECT * from mangosteen_test.ms_userinfo WHERE id={id};
+
+    @Select("SELECT id, username,password,create_time,update_time FROM ms_userinfo WHERE id = #{id}")
+    UserInfo getUserInfoByUserId(Long id);
+
+
+}
